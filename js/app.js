cb25315a0a439ee22233aec5149a971b72e600ef
galaksirehberi <kiyankaya@gmail.com>
2023-03-26 18:27:59 +0300

    saniyenin belirsiz golgesi silindi

--- js/app.js
+++ js/app.js
@@ -14,10 +14,19 @@ Clock.radius = 220;
 Clock.startAngle = 0;
 Clock.endAngle = 0.5 * Math.PI;
 Clock.now = new Date();
+
+// Sec Hand
 Clock.secid = secid;
+Clock.secWidth = 2;
+Clock.secHeight = Clock.radius ;
+
+// Bir kere alinacak degerler
+var digitalClock = Clock.now.getHours() + ':' +  Clock.now.getMinutes()  + ':' +  Clock.now.getSeconds() ; 
+document.getElementById('digital').innerHTML = digitalClock; 
 
 
-    Clock.Shape=()=>{
+
+    Clock.shape=()=>{
         let ctx = Clock.id.getContext("2d");
         ctx.beginPath();
         ctx.fillStyle = "#eb3ad3";
@@ -37,77 +46,37 @@ Clock.secid = secid;
                 } 
                 Vector2.x = Math.sin(i * Math.PI / 180 ) * Clock.radius; 
                 Vector2.y = Math.cos(i * Math.PI / 180 ) * Clock.radius;
+                ctx.fillRect(Vector2.x,Vector2.y, ImSize+2,ImSize+2 );   
+                //console.log(i +'=>' + Vector2.x  +','+ Vector2.y+ '\n'); 
                 
-                ctx.fillRect(Vector2.x,Vector2.y, ImSize+2,ImSize+2 );   // parametreleri radyan cinsinden aliyor.
-               
-                console.log(i +'=>' + Vector2.x  +','+ Vector2.y+ '\n'); 
-                
-                i-=6;  // 2 saniye arasindaki aci derecesi 6 dir. 6*60 cizgi  = 360 derece yapar o da 1 dakika eder.
+                i-=6;  
 
             },10);        
-            
-           // ctx.setTransform(1, 0, 0, 1, 0, 0);
-           // ctx.clearRect(0, 0, 1800, 800);
         ctx.stroke();
     };
 
-    Clock.YmdhHis=()=>{
-
-        let ctx = Clock.id.getContext("2d");
-
-        let c_date = new Date();
-        let hour = c_date.getHours();
-        let minute = c_date.getMinutes();
-        let seconds = c_date.getSeconds();
-
-       let  i = 360 ;
-       let Vector2 = {};
-       
-       let interval = setInterval(function(){
-                    ctx.beginPath(); 
-                   
-                    Vector2.x = Math.sin(i * Math.PI / 180 ) * Clock.radius;   
-                    Vector2.y = Math.cos(i * Math.PI / 180 ) * Clock.radius;   
-                    ctx.moveTo(0,0);
-                    ctx.lineTo(Vector2.x,Vector2.y);       
-                    ctx.stroke();
-                    ctx.closePath();
-                    i-=6;
-                    if( i == 0 ){
-                        i = 360; 
-                    }
-                    console.log(i+'\n');    
 
 
-
-                  
-
-        },1000);  
-        
-            //ctx.setTransform(1, 0, 0, 1, 0, 0);
-            //ctx.rotate(90 * Math.PI / 180 )
-            //ctx.clearRect(0, 0, 800, 800); 
-       
-    };
-
-    Clock.getSec=()=>{
+    Clock.getSec=()=>{ 
         let ctx = Clock.secid.getContext("2d");
-
         let  i = 6 ;
-        let Vector2 = {};
+        
+        //let Vector2 = {};
+        ctx.beginPath();  
         ctx.translate(Clock.coordDx,Clock.coordDy);
-        ctx.beginPath(); 
+        ctx.moveTo(0,0);
+        ctx.fillStyle="RED";
         let interval = setInterval(function(){
-            ctx.clearRect(0, 0, 1800, 800); 
-            Vector2.x = Math.sin(i * Math.PI / 180 ) * Clock.radius;   
-            Vector2.y = Math.cos(i * Math.PI / 180 ) * Clock.radius;   
-            ctx.moveTo(0,0);
-
-            ctx.rotate(i * Math.PI / 180 );       
-            ctx.fillRect(0, 0, 1, Clock.radius);
-            ctx.closePath();
-
-        },1000);      
+            
+            ctx.clearRect(-1, -1, 1800, 800); 
+            //Vector2.x = Math.sin(i * Math.PI / 180 ) * Clock.radius;   
+            //Vector2.y = Math.cos(i * Math.PI / 180 ) * Clock.radius;   
+           
+            ctx.rotate( i * Math.PI / 180 );       
+            ctx.fillRect(0, 0, Clock.secWidth, Clock.secHeight);    
+            ctx.closePath();   
+        },1000);
+          
     }
    
     Clock.Clear=()=>{
@@ -116,22 +85,9 @@ Clock.secid = secid;
             ctx.setTransform(1, 0, 0, 1, 0, 0);
             ctx.clearRect(0, 0, 1800, 800); 
         },2000);
-
     }
 
 
 
-     Clock.Shape(); 
+     Clock.shape(); 
      Clock.getSec();
-   //  Clock.Clear();
- 
- 
-   // Clock.YmdhHis();
- 
-
-   //Clock.Sec();
-   //Clock.Clear();
-   
-   // Clock.MinuteHand();
-   // Clock.HourHand();
-   // Clock.SecHand();
