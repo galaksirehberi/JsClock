b9826e2f02038c34bf319011e59024347ec23731
galaksirehberi <kiyankaya@gmail.com>
2023-03-28 02:35:54 +0300

    dakika ve saat ibresi eklendi

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+let hourid = document.getElementById("ClockHour");
 let minid = document.getElementById('ClockMinute');
 let secid = document.getElementById("ClockSecond");
 let cid = document.getElementById("ClockView");
@@ -21,9 +22,15 @@ Clock.secid = secid;
 Clock.secWidth = 2;
 Clock.secHeight = Clock.radius ;
 
+// Min Hand 
 Clock.minid = minid;
 Clock.minWidth = 10;
 Clock.minHeight = Clock.radius - 100 ;
+
+// Hour Hand
+Clock.hourid = hourid;
+Clock.hourWidth = 15;
+Clock.hourHeight = Clock.radius - 130;
 
 
 // Bir kere alinacak degerler
@@ -32,7 +39,7 @@ document.getElementById('digital').innerHTML = digitalClock;
 
 
 
-    Clock.shape=()=>{
+    Clock.Shape=()=>{
         let ctx = Clock.id.getContext("2d");
         ctx.beginPath();
         ctx.fillStyle = "#eb3ad3";
@@ -63,29 +70,68 @@ document.getElementById('digital').innerHTML = digitalClock;
 
 
 
-    Clock.getSec=()=>{ 
+    Clock.Work=()=>{ 
+        let ctxHour = Clock.hourid.getContext("2d");
+        let ctxMin = Clock.minid.getContext("2d");
         let ctx = Clock.secid.getContext("2d");
         let  i = 6 ;
         
         //let Vector2 = {};
+
+        // hour Hand 
+        ctxHour.beginPath();
+        ctxHour.translate(Clock.coordDx,Clock.coordDy);
+        ctxHour.moveTo(0,0);
+        ctxHour.fillStyle = "GREEN";
+        ctxHour.rotate(Math.PI);                                              // Ibreyi SIFIRLA
+        ctxHour.rotate( i * Clock.now.getHours() * Math.PI / 180 ) ;        // Su andaki dakika konumuna getir.  
+        ctxHour.fillRect(0, 0, Clock.hourWidth, Clock.hourHeight);    
+        ctxHour.closePath();   
+
+
+        // min Hand 
+        ctxMin.beginPath();
+        ctxMin.translate(Clock.coordDx,Clock.coordDy);
+        ctxMin.moveTo(0,0);
+        ctxMin.fillStyle = "ORANGE";
+        ctxMin.rotate(Math.PI);                                              // Ibreyi SIFIRLA
+        ctxMin.rotate( i * Clock.now.getMinutes() * Math.PI / 180 ) ;        // Su andaki dakika konumuna getir.  
+        ctxMin.fillRect(0, 0, Clock.minWidth, Clock.minHeight);    
+        ctxMin.closePath();   
+
+        // Sec Hand 
         ctx.beginPath();  
         ctx.translate(Clock.coordDx,Clock.coordDy);
         ctx.moveTo(0,0);
         ctx.fillStyle="ORANGE";
         ctx.rotate( Math.PI );                                              // ibreyi 6 dan al 12 ye getir. (SIFIRLA)
-        
-                                                                            
-        ctx.rotate( i * Clock.now.getSeconds() * Math.PI / 180 ) ;          // Su andaki saniye konumuna getir.    
+        ctx.rotate( i * Clock.now.getSeconds() * Math.PI / 180 ) ;          // Su andaki saniye konumuna getir.  
+        ctx.fillRect(0, 0, Clock.secWidth, Clock.secHeight);      
 
         let interval = setInterval(function(){
             
-            ctx.clearRect(-1, -1, 1800, 800); 
+            
             //Vector2.x = Math.sin(i * Math.PI / 180 ) * Clock.radius;   
             //Vector2.y = Math.cos(i * Math.PI / 180 ) * Clock.radius;   
            
+            ctx.clearRect(-1, -1, 1800, 800); 
             ctx.rotate( i * Math.PI / 180  );          
             ctx.fillRect(0, 0, Clock.secWidth, Clock.secHeight);    
             ctx.closePath();   
+
+
+            ctxMin.clearRect(-1, -1, 1800, 800); 
+            ctxMin.rotate( i * Math.PI / 180  );          
+            ctxMin.fillRect(0, 0, Clock.minWidth, Clock.minHeight);    
+            ctxMin.closePath();   
+
+            
+            ctxHour.clearRect(-1, -1, 1800, 800); 
+            ctxHour.rotate( i * Math.PI / 180  );          
+            ctxHour.fillRect(0, 0, Clock.hourWidth, Clock.hourHeight);    
+            ctxHour.closePath();   
+          
+            
            
         },1000);
 
@@ -105,8 +151,8 @@ document.getElementById('digital').innerHTML = digitalClock;
 
 
 
-     Clock.shape(); 
-     Clock.getSec();
+     Clock.Shape(); 
+     Clock.Work();
 
 
     
